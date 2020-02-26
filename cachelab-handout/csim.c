fd60be59417bfec22434e3a401d08c630a9aa14e
Louis Ye <yel@carleton.edu>
2020-02-25 20:37:35 -0600

    Update csim.c

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -31,6 +31,22 @@ int hit_count = 0;
 int eviction_count = 0;
 /*****************************************************************************/
 
+void printUsage(char* argv[])
+{
+    printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
+    printf("Options:\n");
+    printf("  -h         Print this help message.\n");
+    printf("  -v         Optional verbose flag.\n");
+    printf("  -s <num>   Number of set index bits.\n");
+    printf("  -E <num>   Number of lines per set.\n");
+    printf("  -b <num>   Number of block offset bits.\n");
+    printf("  -t <file>  Trace file.\n");
+    printf("\nExamples:\n");
+    printf("  linux>  %s -s 4 -E 1 -b 4 -t traces/yi.trace\n", argv[0]);
+    printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", argv[0]);
+    exit(0);
+}
+
 int main(int argc, char* argv[])
 {
   char c;
@@ -65,7 +81,7 @@ int main(int argc, char* argv[])
     /* Make sure that all required command line args were specified */
     if (s == 0 || E == 0 || b == 0 || trace_file == NULL) {
         printf("%s: Missing required command line argument\n", argv[0]);
-        printUsage(argv);
+        //printUsage(argv);
         exit(1);
     }
 
