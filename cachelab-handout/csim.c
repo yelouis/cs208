ee9aba3a00f252d11471ac8497a6b6e0991e2051
Louis Ye <yel@carleton.edu>
2020-02-25 20:25:38 -0600

    Update csim.c

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,63 @@
 #include "cachelab.h"
 
-int main()
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <limits.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[])
 {
-    printSummary(0, 0, 0);
+  char c;
+
+    // Parse the command line arguments: -h, -v, -s, -E, -b, -t
+    while( (c=getopt(argc,argv,"s:E:b:t:vh")) != -1){
+        switch(c){
+        case 's':
+            s = atoi(optarg);
+            break;
+        case 'E':
+            E = atoi(optarg);
+            break;
+        case 'b':
+            b = atoi(optarg);
+            break;
+        case 't':
+            trace_file = optarg;
+            break;
+        case 'v':
+            verbosity = 1;
+            break;
+        case 'h':
+            printUsage(argv);
+            exit(0);
+        default:
+            printUsage(argv);
+            exit(1);
+        }
+    }
+
+    /* Make sure that all required command line args were specified */
+    if (s == 0 || E == 0 || b == 0 || trace_file == NULL) {
+        printf("%s: Missing required command line argument\n", argv[0]);
+        printUsage(argv);
+        exit(1);
+    }
+
+    /* Initialize cache */
+    initCache();
+
+    replayTrace(trace_file);
+
+    /* Free allocated memory */
+    freeCache();
+
+    /* Output the hit and miss statistics for the autograder */
+    printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
