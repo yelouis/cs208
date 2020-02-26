3ae81fdacf3b5c3d8ec47f839ac6e4268389d2aa
Louis Ye <yel@carleton.edu>
2020-02-25 20:35:08 -0600

    Update csim.c

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -11,6 +11,26 @@
 #include <errno.h>
 #include <stdbool.h>
 
+/****************************************************************************/
+/***** DO NOT MODIFY THESE VARIABLE NAMES ***********************************/
+
+/* Globals set by command line args */
+int verbosity = 0; /* print trace if set */
+int s = 0; /* set index bits */
+int b = 0; /* block offset bits */
+int E = 0; /* associativity */
+char* trace_file = NULL;
+
+/* Derived from command line args */
+int S; /* number of sets S = 2^s In C, you can use the left shift operator */
+int B; /* block size (bytes) B = 2^b */
+
+/* Counters used to record cache statistics */
+int miss_count = 0;
+int hit_count = 0;
+int eviction_count = 0;
+/*****************************************************************************/
+
 int main(int argc, char* argv[])
 {
   char c;
