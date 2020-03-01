d4ca2f663853c710d18acbbae97d8e7eb8a10fd1
Louis Ye <yel@carleton.edu>
2020-03-01 14:07:07 -0600

    Update trans.c

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -1,3 +1,5 @@
+//Louis Ye 1999922
+
 /*
  * trans.c - Matrix transpose B = A^T
  *
@@ -23,20 +25,20 @@ char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
   int blockSize;
-  int blockForRow, blockForCol; //to iterate over blocks, user in outer loops
+  int blockRow, blockCol;
   int r, c; //to iterate through each block, used in inner loops
-  int temp = 0, d = 0; //d stands for diagonal, temp is just a temporary variable
-  int v0,v1,v2,v3,v4; //Variables to be used in the N==64 case for various assignments within it
+  int temp = 0, d = 0;
+  int v0,v1,v2,v3,v4;
 	if (N == 32)
 	{
 		blockSize = 8;
-		for(blockForCol = 0; blockForCol < N; blockForCol += 8)
+		for(blockCol = 0; blockCol < N; blockCol += 8)
 		{
-			for(blockForRow = 0; blockForRow < N; blockForRow += 8)
+			for(blockRow = 0; blockRow < N; blockRow += 8)
 			{
-				for(r = blockForRow; r < blockForRow + 8; r++)
+				for(r = blockRow; r < blockRow + 8; r++)
 				{
-					for(c = blockForCol; c < blockForCol + 8; c++)
+					for(c = blockCol; c < blockCol + 8; c++)
 					{
 						if(r != c)
 						{
@@ -49,55 +51,41 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 						d = r;
 						}
 					}
-					if (blockForRow == blockForCol)
+					if (blockRow == blockCol)
 					{
 						B[d][d] = temp;
 					}
 				}
 			}
 		}
-	}
-
-	/* Using blockSize = 4 here.
-	2 levels of loops are used
-	We assign elements in each row individually. Causes reduced missess. */
-	else if (N == 64)
-	{
+	}else if (N == 64){
  		blockSize = 4;
 		for(r = 0; r < N; r += blockSize)
 		{
 			for(c = 0; c < M; c += blockSize)
 			{
-				/*Elements in A[r][], A[r+1][], A[r+2][] are assigned to the variables for use throughout this loop
-				This is becuase we are only allowed to modify the second matrix B but not the matrix A */
 				v0 = A[r][c];
 				v1 = A[r+1][c];
 				v2 = A[r+2][c];
 				v3 = A[r+2][c+1];
 				v4 = A[r+2][c+2];
-				//Elements in B[c+3][] are assigned
 				B[c+3][r] = A[r][c+3];
 				B[c+3][r+1] = A[r+1][c+3];
 				B[c+3][r+2] = A[r+2][c+3];
-				//Elements in B[c+2][] are assigned
 				B[c+2][r] = A[r][c+2];
 				B[c+2][r+1] = A[r+1][c+2];
 				B[c+2][r+2] = v4;
 				v4 = A[r+1][c+1];
-				//Elements in B[c+1][] are assigned
 				B[c+1][r] = A[r][c+1];
 				B[c+1][r+1] = v4;
 				B[c+1][r+2] = v3;
-				//Elements in B[c][] are assigned
 				B[c][r] = v0;
 				B[c][r+1] = v1;
 				B[c][r+2] = v2;
-				//Elements in row A[r+3][] are assigned to the left out elements in B (where B has r+3)
 				B[c][r+3] = A[r+3][c];
 				B[c+1][r+3] = A[r+3][c+1];
 				B[c+2][r+3] = A[r+3][c+2];
 				v0 = A[r+3][c+3];
-				//Finally, elements in row B[c+3][] are assigned
 				B[c+3][r+3] = v0;
 			}
 		}
