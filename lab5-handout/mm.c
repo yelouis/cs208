96409cbd7d0cb3ceb57e2054cb62c7ada9038f9f
Louis Ye <yel@carleton.edu>
2020-03-09 17:35:05 -0500

    Update mm.c

--- lab5-handout/mm.c
+++ lab5-handout/mm.c
@@ -136,7 +136,6 @@ int mm_init(void) {
     //free_listp = heap_start;
 
     //Making a free_list to keep track of all the free blocks
-    free_listp = heap_start;
 
     PUT(heap_start, 0);                        /* alignment padding */
     PUT(PADD(heap_start, 1*WSIZE), PACK(OVERHEAD, 1));  /* prologue header */
@@ -144,6 +143,9 @@ int mm_init(void) {
     PUT(PADD(heap_start, 3*WSIZE), PACK(0, 1));   /* epilogue header */
 
     heap_start = PADD(heap_start, DSIZE); /* start the heap at the (size 0) payload of the prologue block */
+
+    free_listp = heap_start;
+
 
     printf("heap in init\n");
     print_heap();
